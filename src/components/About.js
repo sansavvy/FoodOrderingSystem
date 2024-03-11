6bc8db8614bff1f5224bd471c1773b0b137e5012
sansavvy <vishagasan3108@gmail.com>
2024-03-11 20:38:19 +0530

    class based component life cycle methods

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,25 @@
-import { User } from "./User";
 import UserClass from "./UserClass";
 
-const About = () => {
-  return (
-    <div>
-      <h1>About</h1>
-      <User name={"Lakshmi fnc"} loc={"Chennai, Tamil Nadu"} />
-      <UserClass name={"Lakshmi class"} loc={"Wayand, Kerala"} />
-    </div>
-  );
-};
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("parent constructor");
+  }
+
+  componentDidMount() {
+    console.log("parent component did mount");
+  }
+
+  render() {
+    console.log("parent render");
+    return (
+      <div>
+        <h1>About</h1>
+        <p>This is a About Component</p>
+        <UserClass name={"Lakshmi class"} loc={"Wayand, Kerala"} />
+      </div>
+    );
+  }
+}
 
 export default About;
--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const User = (props) => {
+  const [count, setCount] = useState(0);
+  const [count2] = useState(1);
+
+  useEffect({}, []);
   return (
     <div className="user-card">
+      <h1>Count:{count}</h1>
+      <h1>CountFNC:{count2}</h1>
       <h2>Name: {props.name}</h2>
       <h3>Location: {props.loc}</h3>
       <h4>Contact: vishagasan3108@gmail.com</h4>
